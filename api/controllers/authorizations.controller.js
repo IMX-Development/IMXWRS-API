57844523150b703f1ce19b9d8d6d3c608d0c8fd1
Iván L. Murillo <diskman199@gmail.com>
2021-03-17 12:35:24 -0600

    Authorize waiver and get authorized waivers.

--- api/controllers/authorizations.controller.js
+++ api/controllers/authorizations.controller.js
@@ -3,8 +3,58 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
+function getUsername(req) {
+    return jwt.verify(req.token, process.env.TOKEN_SEED)['username'];
+}
+
+exports.getApproved = (req, res) => {
+    let user = getUsername(req);
+
+    let query = `SELECT number, customer, status, creationDate 
+                FROM  requests WHERE number in (
+                    SELECT TOP 50 request FROM authorizations 
+                    WHERE manager = '${user}' AND signed = 'signed' ORDER BY authorizations.date
+                )`;
+
+    let promise = Sql.request(query);
+
+    promise.then(resp => {
+        res.json({
+            ok: true,
+            authorizations: resp
+        });
+    }, error => {
+        res.json({
+            ok: false,
+            mesage: error
+        });
+    });
+}
+
+exports.authorizeWaiver = (req, res) => {
+    let user = getUsername(req);
+    let waiver = req.body.waiver;
+
+    let query = `UPDATE authorizations SET signed= 'signed', date = CURRENT_TIMESTAMP 
+                WHERE manager = '${user}' AND request = '${waiver}'`;
+
+    let promise = Sql.request(query);
+
+    promise.then(resp => {
+        res.json({
+            ok: true
+        });
+    }, error => {
+        res.json({
+            ok: false,
+            message: error
+        });
+    });
+
+}
+
 exports.getAuthorizations = (req, res) => {
-    let user = jwt.verify(req.token, process.env.TOKEN_SEED)['username'];
+    let user = getUsername(req);
 
     let query = `SELECT requests.customer, users.name as originator, requests.creationDate, 
                 authorizations.* FROM requests, authorizations, users 
--- api/routers/authorizations.routes.js
+++ api/routers/authorizations.routes.js
@@ -3,5 +3,9 @@ const token = require('./../middlewares/authentication');
 
 module.exports = (app) => {
     app.route('/authorizations')
-    .get([token.verifyUser], Authorization.getAuthorizations);
+    .get([token.verifyUser], Authorization.getAuthorizations)
+    .put([token.verifyUser], Authorization.authorizeWaiver);
+
+    app.route('/authorizations/approved')
+    .get([token.verifyUser], Authorization.getApproved);
 }
