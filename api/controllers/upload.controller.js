dd754a8790d209784bd115419df471e108db445c
Iván L. Murillo <diskman199@gmail.com>
2021-04-14 08:37:03 -0500

    Upload file service added
    Just for testing, nothing done yet

---
+++ api/controllers/upload.controller.js
@@ -0,0 +1,42 @@
+var Sql = require('../db/sql.js');
+const status = require('./status.controller');
+const upload = require("../middlewares/upload");
+
+const closeWaiver = async (req, res) => {
+  try {
+    await upload(req, res);
+    console.log('-------------------- NUMBER --------------------');
+    const body = JSON.parse(JSON.stringify(req.body));
+    console.log(body.request);
+    console.log('-------------------- FILES --------------------');
+    console.log(req.files);
+
+    if (req.files.length <= 0) {
+      return res.json({
+        ok: false,
+        message: 'You must select at least 1 file'
+      });
+    }
+    return res.json({
+      ok: true,
+      message: 'Files has been uploaded.'
+    });
+  } catch (error) {
+    console.log(error);
+
+    if (error.code === "LIMIT_UNEXPECTED_FILE") {
+      return res.json({
+        ok: false,
+        message: 'Too many files to upload.'
+      });
+    }
+    return res.json({
+      ok: false,
+      message: `Error when trying upload many files: ${error}`
+    });
+  }
+};
+
+module.exports = {
+  closeWaiver: closeWaiver
+};
--- api/routers/waiver.routes.js
+++ api/routers/waiver.routes.js
@@ -1,9 +1,11 @@
 var Waiver = require('../controllers/waiver.controller');
 const token = require('./../middlewares/authentication');
+const Upload = require('./../controllers/upload.controller');
 
 module.exports = (app) => {
     app.route('/waiver/:waiver')
-    .get(Waiver.getWaiver);
+    .get(Waiver.getWaiver)
+    .post([token.verifyUser], Upload.closeWaiver);
 
     app.route('/waiver')
     .put([token.verifyUser],Waiver.modifyWaiver);
