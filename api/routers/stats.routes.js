ba45a890a935bef1819709bd256d0ddae5198f8e
Iván L. Murillo <diskman199@gmail.com>
2021-06-17 09:24:18 -0500

    Cambio src de imagen en correo

--- api/routers/stats.routes.js
+++ api/routers/stats.routes.js
@@ -4,7 +4,16 @@ module.exports = (app) => {
     app.route('/stats/waivers/:filter')
     .get(Stats.getWaiverStats);
 
-    app.route('/stats/data/:filter')
+    app.route('/stats/data/waivers')
+    .get(Stats.getData);
+
+    app.route('/stats/data/remarks')
+    .get(Stats.getData);
+
+    app.route('/stats/data/actions')
+    .get(Stats.getData);
+
+    app.route('/stats/data/authorizations')
     .get(Stats.getData);
 
 }
